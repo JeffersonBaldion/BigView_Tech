30dab646a198c742cf3203dbb59a2af36de1597a
Jefferson Baldion <jefferson.baldion.b@gmail.com>
2024-04-30 08:04:43 -0500

    update vite config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Directorio de salida para los archivos compilados
+    minify: true,   // Minificar el código en producción
+    sourcemap: false, // Deshabilitar los mapas de origen en producción (mejora el rendimiento)
+  }
 })
