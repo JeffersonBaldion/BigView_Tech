35f3c9f909e20330cf48d4a99c725fec58a4d25b
Jefferson Baldion <jefferson.baldion.b@gmail.com>
2024-04-30 07:44:42 -0500

    index fixed

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db/config.js");
-const productRouter = require("./Router/productRouter");
+const productRouter = require("./Router/productRouter.js");
 const userRouter = require("./Router/userRouter.js");
 const cartRouter = require("./Router/cartRouter.js");
 
@@ -12,14 +12,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// app.use("/", (req, res)=>{
-//     res.status(200).send("Hello Big View")
-// })
+
 app.use("/user", userRouter);
 app.use("/product", productRouter);
 app.use("/cart", cartRouter);
 
 app.use(express.static("uploads"));
+
+app.use("/", (req, res)=>{
+    res.status(200).send("Hello Big View")
+})
 
 connectDB();
 
